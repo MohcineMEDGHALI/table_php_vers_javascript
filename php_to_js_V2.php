30a89df818e267fca49db199256fcfad0e6ec6c9
MEDGHALI_Mohcine <medghali.mohcine@gmail.com>
2018-12-10 16:34:34 +0100

    Rien de spéciale juste suppresion de quelque ligne de code unitiles

--- php_to_js_V2.php
+++ php_to_js_V2.php
@@ -8,42 +8,34 @@ class Personne{
         $this->prenom = $prenom;
     }
 }
-
-$prs =array(
-    new Personne(12,'MEDGHALI','Mohcine'),
-    new Personne(13,'ENNAJEM','Yassine'),
-    new Personne(14,'SKOUZA','Skouza')
-    ) ;
 $scriptData = Array('resultCode'=>200,'database'=>array());
     foreach($prs as $p){
         $scriptData['database'][] = get_object_vars($p);
     }
-    //'database' => get_object_vars($prs)
-  
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Data from PHP to JS</title>
     <script type="text/javascript">
         scriptData = <?php echo json_encode($scriptData); ?>;
     </script>
 </head>
 <body>
-    
-<button id="btn">Bouton</button>
+    <button id="btn">Bouton</button>
 
-  <script>
-    alert(scriptData.resultCode);
-    document.getElementById('btn').addEventListener("click",function () {
-        if (200 == scriptData.resultCode) {
-        alert('user name : ' + scriptData.database[1].nom+" "+scriptData.database[1].prenom);
-        } else {
-        alert('invalid Query !');
-        }
-    });
-  </script>
+    
+    <script>
+        alert(scriptData.resultCode);
+        document.getElementById('btn').addEventListener("click",function () {
+            if (200 == scriptData.resultCode) {
+            alert('user name : ' + scriptData.database[1].nom+" "+scriptData.database[1].prenom);
+            } else {
+            alert('invalid Query !');
+            }
+        });
+    </script>
 
 </body>
 </html>
