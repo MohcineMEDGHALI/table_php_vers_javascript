a0a342ae6c37c8144519082b53ac594ff17964a8
MEDGHALI_Mohcine <medghali.mohcine@gmail.com>
2018-12-26 13:12:48 +0100

    quelque modification pour simplifier le code de fichier php_to_js_V2.php et aussi l'ajout des commentaires

--- php_to_js_V2.php
+++ php_to_js_V2.php
@@ -1,4 +1,5 @@
 <?php
+    #Déclation d'une simple classe
     class Personne{
         public $id,$nom,$prenom;
         public function __construct($id,$nom,$prenom)
@@ -8,14 +9,18 @@
             $this->prenom = $prenom;
         }
     }
-    $scriptData = Array('resultCode'=>200,'database'=>array());
-
-    $prs = array(new Personne(1,"Nom 1","Prénom 1"),
-                    new Personne(2,"Nom 2","Prénom 2"),
-                    new Personne(3,"Nom 3","Prénom 3"));
-    foreach($prs as $p){
-        $scriptData['database'][] = get_object_vars($p);
-    }
+    /*
+    Déclarartion d'une table qui contient une variable pour le teste dans la case 0 
+    et une table qui contient des variables de type Personne
+    */
+    $MesDonneesPhp = Array(
+        'resultCode'=>200, #variable pour le teste
+        #table des personnes
+        'database'=>array(new Personne(1,"Nom 1","Prénom 1"),
+                          new Personne(2,"Nom 2","Prénom 2"),
+                          new Personne(3,"Nom 3","Prénom 3")
+                          )
+    );
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,7 +28,8 @@
         <meta charset="UTF-8">
         <title>Data from PHP to JS</title>
         <script type="text/javascript">
-            scriptData = <?php echo json_encode($scriptData); ?>;
+            //Récupérer notre table MesDonneesPhp qu'on a créer
+           var mesDonneesJs = <?php echo json_encode($MesDonneesPhp); ?>;
         </script>
     </head>
     <body>
@@ -33,15 +39,23 @@
         
         <script>
             document.getElementById("btn").addEventListener("click",function(){
-                if(200 == scriptData.resultCode){
+                //tester la valeur de notre varieble du teste
+                if(200 == mesDonneesJs.resultCode){
+                    //Créer un élément ul 
                     var ul = document.createElement("ul");
-                    for(var i=0;i<scriptData.database.length;i++){
+                    //Parcourir nos données (personnes)
+                    for(var i=0;i<mesDonneesJs.database.length;i++){
+                        //Créer un élément li 
                         var li = document.createElement("li");
-                        li.textContent="id : " + scriptData.database[i].id+" / Nom : "+scriptData.database[i].nom+" / Prénom : "+scriptData.database[i].prenom;
+                        //Ajouter du text au dernière li crée
+                        li.textContent="id : " + mesDonneesJs.database[i].id+" / Nom : "+mesDonneesJs.database[i].nom+" / Prénom : "+mesDonneesJs.database[i].prenom;
+                        //Ajouter le dernière li crée à notre l'élément ul
                         ul.appendChild(li);
                     }
+                    //Ajouter notre l'élément ul à un div dans notre page
                     document.getElementById("liste").appendChild(ul);
-                } else {
+                }
+                else{
                 alert('invalid Query !');
                 }
             });
